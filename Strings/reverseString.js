ce5e2ad6bdb24fe66c9155ac7120451188f3d21d
sumit-dev-ai <sumit.ai.dev@gmail.com>
2025-07-03 20:21:07 +0530

    3 jul

--- Strings/reverseString.js
+++ Strings/reverseString.js
@@ -1,1 +1,13 @@
-yg
+var reverseString = function (s) {
+
+    let leftP = 0;
+    let rightP = s.length - 1;
+    while (leftP < rightP) {
+        [s[leftP], s[rightP]] = [s[rightP], s[leftP]]
+        leftP++;
+        rightP--;
+
+    }
+    return s;
+};
+console.log(reverseString(["h","e","l","l","o"]));
